c9165a1d0f67f3d136b1dbc01e0eb7b90543c9b3
Akira <coruscationmail@gmail.com>
2024-09-16 18:24:22 +0800

    flake for node build

--- flake.nix
+++ flake.nix
@@ -1,19 +1,22 @@
+
 {
-  description = "Java development environment";
+  description = "";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-          fhsFn = runScript: pkgs.buildFHSUserEnv {
-            name = "espoir";
-            targetPkgs = pkgs: [pkgs.gcc pkgs.libz] ;
-            runScript = runScript;
-          };
       in
       {
-        devShells.default = (fhsFn "/usr/bin/env bash").env;
-        packages.default = (fhsFn "${pkgs.babashka}/bin/bb ${self}/espoir");
+        # devShells.default = (fhsFn "/usr/bin/env bash").env;
+        packages.default = pkgs.buildNpmPackage rec {
+          pname = "espoir";
+          version = "0.0.1";
+          src = "${self}";
+          npmDepsHash = "sha256-z5gH2YEoDBuNs8MD9L0Rs9UcPmreiiKnSM6WZla66ng=";
+          npmPackFlags = [ "--ignore-scripts" ];
+          dontNpmBuild = true;
+        };
       }
     );
 }
